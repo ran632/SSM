cf7e6f9666feecae108731179c9a42fa09c0003c
ran632 <ran632@gmail.com>
2015-05-27 00:04:18 +0300

    login2 - testing

---
+++ web/pages/LoginTest.py
@@ -0,0 +1,78 @@
+from google.appengine.ext.webapp import template
+
+import webapp2
+import json
+from models.user import User
+
+class LoginTestHandler(webapp2.RequestHandler):
+    def get(self):
+        user = None
+        if self.request.cookies.get('our_token'):    #the cookie that should contain the access token!
+            user = User.checkToken(self.request.cookies.get('our_token'))
+
+        template_variables = {}
+        if user:
+            template_variables['user'] = user.email
+
+        html = template.render('web/templates/LoginTest.html', template_variables)
+        self.response.write(html)
+
+class Login2Handler(webapp2.RequestHandler):
+    def get(self):
+        email = self.request.get('email')
+        password = self.request.get('password')
+        user = User.query(User.email == email).get()
+        if not user or not user.checkPassword(password):
+            self.error(403)
+            self.response.write('Wrong username or password')
+            return
+
+        self.response.set_cookie('our_token', str(user.key.id()))
+        self.response.write(json.dumps({'status':'OK'}))
+
+class RegisterHandler(webapp2.RequestHandler):
+    def get(self):
+        email = self.request.get('email')
+        password = self.request.get('password')
+        if not password:
+            self.error(403)
+            self.response.write('Empty password submitted')
+            return
+
+        user = User.query(User.email == email).get()
+        if user:
+            self.error(402)
+            self.response.write('Email taken')
+            return
+
+        user = User()
+        user.email = email
+        user.setPassword(password)
+        user.put()
+        self.response.set_cookie('our_token', str(user.key.id()))
+        self.response.write(json.dumps({'status':'OK'}))
+
+class LogoutHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.delete_cookie('our_token')
+        self.redirect('/')
+
+class PersonalHandler(webapp2.RequestHandler):
+    def get(self):
+        user = None
+        if self.request.cookies.get('our_token'):    #the cookie that should contain the access token!
+            user = User.checkToken(self.request.cookies.get('our_token'))
+
+        if not user:
+            self.redirect('/')
+
+        html = template.render('web/templates/History.html', {})
+        self.response.write(html)
+
+app = webapp2.WSGIApplication([
+    ('/LoginTest', LoginTestHandler),
+    ('/login2', Login2Handler),
+    ('/register', RegisterHandler),
+    ('/logout', LogoutHandler),
+    ('/personal', PersonalHandler)
+], debug=True)
