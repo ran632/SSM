a4c536b92651cb0c8e22273aa89e1bca7647e149
ran632 <ran632@gmail.com>
2015-08-12 18:14:27 +0300

    printable version - in 'history'

--- web/static/js/history.js
+++ web/static/js/history.js
@@ -2,4 +2,13 @@ $(function() {  //this is jQuery's short notation for "fire all this when page i
 	$('#cal').on('change', function () {
 		location.href = '/History?date=' + $('#cal').val();
 	});
+	$('#prnt').on('click', function () {
+		$('body > :not(.printversion)').hide(); //hide all nodes directly under the body
+		$('.printversion').appendTo('body');
+		
+		$('.success').removeClass( 'success' );
+		$('.info').removeClass( 'info' );
+		$('.warning').removeClass( 'warning' );
+		$('.danger').removeClass( 'danger' );
+	});
 });
--- web/static/js/switchshifts.js
+++ web/static/js/switchshifts.js
@@ -32,7 +32,7 @@ function submitSwitch(){
 	var from_shift_id = $('#myshifts').find('option:selected').attr('id');
 	var to_empno = $('#emplist').find('option:selected').attr('id');
 	var to_shift_id = $('#othershifts').find('option:selected').attr('id');
-	
+
 	$.ajax({
 		url:'/SwitchShifts/switchAtt',
 		type:'GET',
